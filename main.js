5db78a29fecd22136cd5ca81e4253a4eedc7cd09
Divya Darshan <parukrish.darshan20@gmail.com>
2024-12-25 17:01:55 +0530

    add and updated the footer with copy rights

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // JavaScript
 const video = document.getElementById("myVideo");
 const audioButton = document.getElementById("audioButton");
+const navMenu = document.getElementById("nav-menu");
 
 // Toggle mute/unmute on button click
 audioButton.addEventListener("click", () => {
@@ -12,3 +13,20 @@ audioButton.addEventListener("click", () => {
     audioButton.innerHTML = '<i style="color: white;" class="fa-solid fa-volume-xmark"></i>'; // Muted icon
   }
 });
+
+const ham = document.getElementById("ham");
+
+
+
+function toggleNavMenu() {
+    navMenu.classList.toggle("active"); // Toggles the "active" class on the menu
+}
+
+ham.addEventListener("click", toggleNavMenu);
+
+function toggleNavMenu() {
+    navMenu.classList.toggle("active"); // Toggles the "active" class on the menu
+    ham.classList.toggle("rotate"); // Toggles the "rotate" class on the icon
+}
+
+ham.addEventListener("click", toggleNavMenu);
